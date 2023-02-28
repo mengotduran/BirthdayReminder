1922dd1a375c66227c7fedfa9485f0971f8bf574
mengotduran <65475728+mengotduran@users.noreply.github.com>
2023-02-28 19:25:44 +0100

    initial commit

---
+++ src/List.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const List = ({people}) => {
+  return (
+    <>
+      {people.map((person) => {
+        const {id, name, age, image} = person;
+        return <article key={id} className="person">
+          <img src={image} alt={name} />
+          <div>{name}</div>
+          <p>{age} years</p>
+        </article>
+      })}
+    </>
+  );
+};
+
+export default List;
